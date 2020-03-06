c0943734d10ea14d81588668fd9d605821529cff
Rasmus Kröger <rasmus.kroger@suoratyo.fi>
2020-03-06 14:55:20 +0200

    Remove ejs

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const express = require('express');
-const ejs = require('ejs');
 
 const port = process.env.PORT || 8080;
 const app = express();
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const venueMap = {
